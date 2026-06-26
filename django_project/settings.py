cc731f4073c8fe3603b18eda9a3e2d9ab33a3db9
carolina <carolinacecchi06@gmail.com>
2026-06-26 18:19:46 +0200

    Add CustomUser

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',
-    'events.apps.EventsConfig',
-    'registrations.apps.RegistrationsConfig',
+    'accounts',
+    'events',
+    'registrations',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
