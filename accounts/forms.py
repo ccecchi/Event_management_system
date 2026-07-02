ba4a09ae7dca078000334d71a24c672309cda856
carolina <carolinacecchi06@gmail.com>
2026-07-02 19:36:53 +0200

    Minor template fixes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = UserCreationForm._meta.fields + ("email", "first_name", "last_name")
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
