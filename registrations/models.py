a32550a98e3dfcc6091285c0a61aa3aa506ad30c
carolina <carolinacecchi06@gmail.com>
2026-07-03 18:13:53 +0200

    Add registration model

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from events.models import Event
+from accounts.models import CustomUser
 
-# Create your models here.
+class Registration(models.Model):
+    attendee = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('attendee', 'event')
+
+    def __str__(self):
+        return f'{self.event.title} - {self.attendee.username}'
